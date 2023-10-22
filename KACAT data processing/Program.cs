8fd3aafbcf06582d269b4da8cc6abcb716a860fd
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-10-22 09:44:55 +0200

    create Visual Studio project

---
+++ KACAT data processing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KACAT_data_processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
