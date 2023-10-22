d7b56fe286d5d91b18a1e119d7475aebd8758a5a
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-10-22 11:40:36 +0200

    setup gps variables for ArduinoSim class

---
+++ KACAT data processing/ArduinoSim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KACAT_data_processing
+{
+    internal class ArduinoSim
+    {
+        // GPS data
+        private double lat, lon, altitude, hGeoid;
+        private char latDir, lonDir, altitudeUnit, hGeoidUnit;
+        private string gpsIdentifier = "$GPGGA";
+        private char checksumEnd = '*';
+
+        public ArduinoSim()
+        {
+
+        }
+    }
+}
